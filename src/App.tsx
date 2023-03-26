298da2dc89b40d83054eaa83b6aa33cbc35d2b76
s1f102100793 <85666759+s1f102100793@users.noreply.github.com>
2023-03-27 01:10:51 +0900

    61 カスタムフックを使わないデータ取得例

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,52 @@
 import "./styles.css";
+import { Usercard } from "./components/Usercard";
+import axios from "axios";
+import { User } from "./types/api/user";
+import { UserProfile } from "./types/userProfile";
+import { useState } from "react";
 
 export default function App() {
+  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+  const [loading, setloading] = useState(false);
+  const [error, setError] = useState(false);
+
+  const onClickFetchUser = () => {
+    setloading(true);
+    setError(false);
+
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.street}`
+        }));
+        setUserProfiles(data);
+      })
+      .catch(() => {
+        setError(true);
+      })
+      .finally(() => {
+        setloading(false);
+      });
+  };
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <button onClick={onClickFetchUser}>データ取得</button>
+      <br />
+      {error ? (
+        <p style={{ color: "red" }}>データの取得に失敗しました</p>
+      ) : loading ? (
+        <p>loading...</p>
+      ) : (
+        <>
+          {userProfiles.map((user) => (
+            <Usercard key={user.id} user={user} />
+          ))}
+        </>
+      )}
     </div>
   );
 }
